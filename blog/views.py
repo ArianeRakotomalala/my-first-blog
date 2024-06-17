632308a5501aa3ffd25f3ab520baec9e2d702b4a
ariane <arianerakotomalala@gmail.com>
2024-06-17 15:08:33 +0300

    Modificaiton du fichier Html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Comme vous pouvez le voir, nous avons créé une fonction (def) appelée post_list qui prend une request
+# (requête) et qui va return (retourner) la valeur donnée par une autre fonction render qui va 
+# assembler notre template blog/post_list.html.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
